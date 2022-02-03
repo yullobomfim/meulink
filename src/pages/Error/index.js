ebb9d183d8522f6e631a6c34e600ba0b006f5239
yullo.bomfim <yullo.bomfim@gmail.com>
2022-02-03 17:43:06 -0300

    HandleButton

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,8 +1,13 @@
 import './error.css';
 import { Link} from 'react-router-dom';
 
+
+
 export default function Error(){
-return(
+
+
+
+    return(
     <div className="container-error">
     <img src="notfound.png" alt="Page not found"/>
         <h1>Page not found</h1>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,32 @@
 import { FiLink } from 'react-icons/fi'
 import './home.css'
 import Menu from '../../components/menu'
+import { useState} from 'react';
+
 
 export default function Home() {
+const [link, setLink] = useState('');
+
+  function handleShortLink(){
+    alert( "My link is "+ link)
+  }
   return (
     <div className="container-home">
       <div className="logo">
         <img src="/logo.png" alt="Link Logo" />
         <h1>Home</h1>
-        <span> Cole um link para encurtar</span>
+        <span> Paste your link to shorten</span>
       </div>
       <div className="area-input">
         <div>
           <FiLink size={24} color='#FFF' />
-          <input placeholder='Cole seu link aqui' />
+          <input 
+          placeholder='Paste your link here' 
+          value={link}
+          onChange={ (e) => setLink(e.target.value)}
+          />
         </div>
-        <button>Encurtar Link</button>
+        <button onClick={handleShortLink}>Shorten link</button>
       </div>
       <Menu/>
     </div>
